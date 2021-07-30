77cdd51d6e7bedb65417a46d053037dc7c5bf2a7
Umer Hurramov <umer.hurramov@gmail.com>
2021-07-30 14:12:09 +0300

    remove https from the skip links

--- src/javascripts/page-transition.js
+++ src/javascripts/page-transition.js
@@ -4,7 +4,7 @@ export default function initPageTransition(router) {
   $('body').on('click', 'a[href]', (e) => {
     const href = e.currentTarget.getAttribute('href');
     // console.log(href, e.currentTarget);
-    if (!['#', 'mailto', 'tel', 'https'].some((skip) => href.indexOf(skip) > -1)) {
+    if (!['#', 'mailto', 'tel'].some((skip) => href.indexOf(skip) > -1)) {
       router.navigate(href);
       e.preventDefault();
     }
